c8b3d8087aa8afb5a5676d2f868b914ba12bced2
wenxiong <1023308356@qq.com>
2020-12-08 11:48:31 +0800

    1.0.0

--- test/quick_test.dart
+++ test/quick_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:quick/quick.dart';
+import 'package:quick_layout/quick.dart';
 
 void main() {
   test('adds one to input values', () {
